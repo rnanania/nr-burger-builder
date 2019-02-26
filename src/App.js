3e9bbb9e6be5b249a3e74e74b0a4c700e91123bf
Rohit Nanania <nanania.rohit@gmail.com>
2019-02-26 10:45:53 -0500

    Font awesome addition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import logo from './logo.svg';
 import './App.css';
+
+// Font awesome icons addition to app.
+library.add(faHome);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <FontAwesomeIcon icon="home" size="lg" title="Home"/>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );

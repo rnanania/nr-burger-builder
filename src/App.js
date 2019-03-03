94abacefecea436b117b45716964c06bf1711b52
Rohit Nanania <nanania.rohit@gmail.com>
2019-03-03 01:01:50 -0500

    Lazy loading addition.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 // Require imports for font awesome icons to use in react app.
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHamburger, faBars, faSpinner, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import Spinner from './components/UI/Spinner/Spinner';
 import './App.css';
 
 import * as actions from './store/actions/index';
 import Layout from './containers/Layout/Layout';
+
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
-import Logout from './containers/Auth/Logout/Logout';
 
 // Font awesome icons addition to app.
 library.add(faBars, faHamburger, faSpinner, faExclamationCircle);
+
+const Orders = React.lazy(() => import('./containers/Orders/Orders'));
+const Logout = React.lazy(() => import('./containers/Auth/Logout/Logout'));
 
 class App extends Component {
   componentDidMount() {
@@ -30,9 +33,9 @@ class App extends Component {
           <Switch>
             {this.props.isAuthenticated ? <Route path="/checkout" component={Checkout} /> : null}
             <Route path="/builder" component={BurgerBuilder} />
-            {this.props.isAuthenticated ? <Route path="/orders" component={Orders} /> : null}
+            {this.props.isAuthenticated ? <Route path="/orders" render={() => <Suspense fallback={<Spinner />}><Orders /></Suspense>} /> : null}
             <Route path="/login" component={Auth} />
-            {this.props.isAuthenticated ? <Route path="/logout" component={Logout} /> : null}
+            {this.props.isAuthenticated ? <Route path="/logout" render={() => <Suspense fallback={<Spinner />}><Logout /></Suspense>} /> : null}
             <Redirect from="/" to="/builder" />
           </Switch>
         </Layout>
